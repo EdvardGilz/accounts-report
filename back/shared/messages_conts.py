0a54f4b06d1dcf317cb878d99232edf8f48c64f0
Eduardo Giles <>
2024-06-25 18:44:20 -0600

    Add misc functions and const

---
+++ back/shared/messages_conts.py
@@ -0,0 +1,11 @@
+FILE_ERROR_MSG = 'Archivo inexistente o corrupto.'
+EMPTY_FILE_MSG = 'Archivo vacío.'
+MISSING_COLUMNS_MSG = 'Columnas faltantes: '
+ID_COLUMN_FORMAT_ERROR_MSG = 'Error en la columna id, debe ser un entero.'
+TRANSACTION_COLUMN_FORMAT_ERROR_MSG = (
+    'Error en la columna transaction, debe ser un valor numérico.'
+)
+DATE_FORMAT_ERROR_MSG = (
+    'Formato de fecha incorrecto, el formato debe ser D/MM/YYYY.'
+)
+DUPLICATED_IDS_ERROR_MSG = 'Los siguientes ids están duplicados en el archivo: '
